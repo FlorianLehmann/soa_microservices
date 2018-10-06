25aa7c3c8cf4b436b376fa3ddc26a4cf3b83ef76
FlorianLehmann <Lehmann.florian@yandex.com>
2018-10-06 09:37:39 +0200

    Linked a database to the catalogue service

--- catalogue/src/main/java/microservices/CatalogueRESTImpl.java
+++ catalogue/src/main/java/microservices/CatalogueRESTImpl.java
@@ -2,14 +2,20 @@ package microservices;
 
 import model.Product;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.ws.rs.Path;
 import java.util.ArrayList;
 import java.util.Collection;
 
 @Path("rest/catalogue")
 public class CatalogueRESTImpl implements CatalogueREST {
+
+    @PersistenceContext(unitName="catalogue")
+    private EntityManager entityManager;
+
     @Override
     public Collection<Product> listProducts() {
-        return new ArrayList<>();
+        return entityManager.createQuery("select p from Product p").getResultList();
     }
 }
---
+++ catalogue/src/main/resource/postgresql-catalogue/init.sql
@@ -0,0 +1,6 @@
+CREATE TABLE products (
+    id integer PRIMARY KEY,
+    name varchar(50)
+);
+
+INSERT INTO products VALUES (0, 'ramen soup');

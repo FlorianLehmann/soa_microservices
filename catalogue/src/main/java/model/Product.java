67381012646917c0dd7a4946de836fda09522fd9
FlorianLehmann <Lehmann.florian@yandex.com>
2018-10-06 09:32:05 +0200

    add attributes to Product (id and name)

--- catalogue/src/main/java/model/Product.java
+++ catalogue/src/main/java/model/Product.java
@@ -1,4 +1,25 @@
 package model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="products")
 public class Product {
+
+    private int id;
+    private String name;
+
+    public Product() {
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
 }
